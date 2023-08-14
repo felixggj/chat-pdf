903a2d35d8dd3b38fbc4398b0665b790706dfcf5
felixggj <me@felix-gomez.com>
2023-08-14 18:44:45 +0200

    Setting OpenAI API and streamlit app structure

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
+
+
+
+
+if __name__ == '__main__':
+    main()
