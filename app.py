2241c95779e2d3b97ee71171ebde56fa5b058d4f
felixggj <me@felix-gomez.com>
2023-08-18 11:44:22 +0200

    Created a conversation chain using langchain

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 
 def get_pdf_text(pdf_docs):
@@ -32,10 +35,23 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
@@ -56,6 +72,9 @@ def main():
 
                 # Creating vector store
                 vectorstore = get_vectorstore(text_chunks)
+
+                # Conversation Chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 
