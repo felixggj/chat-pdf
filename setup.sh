5cec9fdeec64c7ace2e370e2e812623137492d8a
felixggj <me@felix-gomez.com>
2023-08-21 19:35:14 +0200

    Testing Heroku deployment error

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"youremail@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
+
